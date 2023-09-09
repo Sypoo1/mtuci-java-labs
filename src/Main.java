448f8e588c055b626033e26ac441622abd5485da
Sypoo1 <h89672142336@yandex.ru@>
2023-09-09 19:45:02 +0300

    create new method in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        bye();
+    }
+    public static void bye(){
+        System.out.println("Bye!");
     }
 }
