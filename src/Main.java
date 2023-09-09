f47e26817cdce24d4b53d9064b68feb808960d2d
Sypoo1 <h89672142336@yandex.ru@>
2023-09-09 20:32:21 +0300

    create new method summa

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         bye();
+        System.out.println(summa(5,5));
     }
     public static void bye(){
         System.out.println("Bye!");
+    }
+    public static int summa(int a, int b){
+        return a + b;
     }
 }
